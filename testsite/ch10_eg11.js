6956f42dfd8c402d758333779011ecdebc094a0b
Cpotter2 <36063358+Cpotter2@users.noreply.github.com>
2018-06-18 19:15:38 -0400

    Add
    Add Javascript assignment page, add link from homepage, clean up footer of homepage.

---
+++ testsite/ch10_eg11.js
@@ -0,0 +1,7 @@
+var myString = "Learning about Built-in Objects is easy";
+myString = myString.substring(15, 31);
+myString = myString.toUpperCase();
+document.body.innerHTML += "<h1>" + myString + "</h1>";
+
+document.body.innerHTML += "<table id=\"tester\"style=\"border:2px solid black; cellpadding: 2px;\"><caption>This might suck a bit</caption><tr><th>Roses are red</th><th>Violets are blue</th> \
+</tr><tr><td>I'm going crazy</td><td>(Crazy for you)</td></tr></table>"
---
+++ testsite/script.js
@@ -0,0 +1,3 @@
+var x = 23;
+document.write(x + 4);
+document.write("<br />That is my favorite number!");
